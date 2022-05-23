6d031f20b684e866a303278f35ae5917c6e4f973
Rahulhawlader <hawladerrahul8@gmail.com>
2022-05-23 10:40:03 +0600

    server site first get

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { query } = require('express');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7jjlo.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const productCollection = client.db('Bycycle-store').collection('products');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Hello Bicycle World!')
+})
+
+app.listen(port, () => {
+    console.log(`Bicycle app listening on port ${port}`)
+})
